75f781fb3af03bc78cd3fa84c6438e1de23e0d66
Steffany Pachall <steffany@Peters-MacBook-Pro.local>
2016-05-31 16:28:00 -0700

    new movie project

---
+++ media.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+class Movie(object):
+    """instances of the class Movie / the data/instance variables that the class contains"""
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
